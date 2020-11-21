1ec8c88081b7f95777e4a6bf4aa2c6cf481948b2
Hashem-Poya <poya.kpu.2020@gmail.com>
2020-11-21 10:58:17 -0800

    Renaming script to openpyxl_python_script.py and creating another script by name of xlrd_python_script.py, (openpyxl_python_script.py developed using openpyxl library, xlrd_python_script.py developed using xlrd library.

---
+++ xlrd_python_script.py
@@ -0,0 +1,76 @@
+import os
+import base64
+import xmlrpc.client
+import psycopg2
+import time
+from tqdm import tqdm
+import xlrd
+
+workbook = xlrd.open_workbook(filename='excel_file.xlsx')
+worksheet = workbook.sheet_by_index(0)
+MAX_ROW = worksheet.nrows
+
+url = 'http://localhost:8000'
+db = 'inventory_db'
+username = 'admin'
+password = 'admin'
+
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+uid = common.authenticate(db, username, password, {})
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+
+def db_connection():
+    
+    USER = 'ubuntu'
+    PASSWORD = '1234'
+    HOST = '127.0.0.1'
+    PORT = ''
+    DATABASE = 'inventory_db'
+    connection = None
+
+    try:
+        connection = psycopg2.connect(user=USER, password=PASSWORD, host=HOST, port=PORT, database=DATABASE)
+    except (Exception, psycopg2.Error) as error:
+        print('Error while connection to postgres', error)
+
+    return connection
+
+connection = db_connection()
+cursor = connection.cursor()
+
+def get_commodity_group_id(commodity_value):
+    if commodity_value:
+        cursor.execute("""SELECT id FROM product_commodity_group WHERE name = %s """ ,(commodity_value,))
+        commodity_group_id = cursor.fetchone()
+        return commodity_group_id
+    else:
+        return None
+
+def encode_image(image_value):
+    try:
+        if image_value:
+            img = open('photos/{img}'.format(img=image_value), 'rb')
+            im_b64 = base64.b64encode(img.read()).decode('utf-8')
+            return im_b64
+        else:
+            return ''
+    except:
+        return ''
+
+
+for row in tqdm(range(2, MAX_ROW)):
+    item = worksheet.cell_value(row, 0)
+    description = worksheet.cell_value(row,1)
+    part_number = worksheet.cell_value(row, 2)
+    commodity_group_id = get_commodity_group_id(worksheet.cell_value(row, 3))
+    encoded_img = encode_image(worksheet.cell_value(row, 4))    
+    models.execute_kw(db, uid, password, 'product.template', 'create', [{
+            'default_code': item,
+            'name': description,
+            'commodity_group': commodity_group_id[0] if commodity_group_id else 0,
+            'image_1920': encoded_img,
+        }])
+
+connection.close()
+
+
